e6c0d35628f1a93f386414dc34d80640994ada79
onpul <choionpul@gmail.com>
2023-12-08 15:11:00 +0900

    [개인] router 세팅 테스트

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
-import TodoTemplate from './components/TodoTemplate';
-import TodoHead from './components/TodoHead';
-import TodoList from './components/TodoList';
-import TodoCreate from './components/TodoCreate';
-import { TodoProvider } from './TodoContext';
-import Users from './Users';
+import { Route, Routes } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
 
-// import logo from './logo.svg';
-// import './App.css';
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    background: #e9ecef;
-  }
-`;
-
-function App() {
+const App = () => {
     return (
-        <Users />
-        // <TodoProvider>
-        //     <GlobalStyle />
-        //     <TodoTemplate>
-        //         <TodoHead />
-        //         <TodoList />
-        //         <TodoCreate />
-        //     </TodoTemplate>
-        // </TodoProvider>
+        <div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </div>
     );
-}
+};
 
 export default App;
