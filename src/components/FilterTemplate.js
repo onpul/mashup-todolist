66549e3129ed4d363db895b0323ef656273487d6
onpul <choionpul@gmail.com>
2023-12-19 17:05:22 +0900

    [개인] 버튼용 컴포넌트 생성, 추후 구현할 기능 구상 기록

--- src/components/FilterTemplate.js
+++ src/components/FilterTemplate.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const TodoTemplateBlock = styled.div`
+const FilterTemplateBlock = styled.div`
     width: 512px;
     height: 768px;
 
-    position: relative; /* 추후 박스 하단에 추가 버튼을 위치시키기 위한 설정 */
+    position: relative;
     background: white;
     border-radius: 16px;
     box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
@@ -15,8 +15,8 @@ const TodoTemplateBlock = styled.div`
     flex-direction: column;
 `;
 
-function TodoTemplate({ children }) {
-    return <TodoTemplateBlock>{children}</TodoTemplateBlock>;
+function FilterTemplate({ children }) {
+    return <FilterTemplateBlock>{children}</FilterTemplateBlock>;
 }
 
-export default TodoTemplate;
+export default FilterTemplate;
